215fc518317bac5387faaf86bde5f90e78af2ca6
dalborgo <marco@dalborgo.it>
2017-04-26 12:28:54 +0200

    a3

--- footer.php
+++ footer.php
@@ -1,12 +1,16 @@
 <!-- footer  style-->
+
 <footer>
     <section class="clearfix footer-wrapper">
         <section class="container clearfix footer-pad">
+
             <div class="widget about-us-widget col-md-4 col-sm-6">
                 <a href="index.html"><img src="images/footer/logo.png" alt="" class="img-responsive"></a>
-                <p>Posto all’interno di un grande parco di Ulivi Secolari (1500 mq), a pochi passi da centro storico di
-                    Garda e a 300 metri dalle spiagge del Lago.</p>
+
+                <p>Posto all’interno di un grande parco di Ulivi Secolari (1500 mq), a pochi passi da centro storico di Garda e a 300 metri dalle spiagge del Lago.</p>
+
             </div>
+
             <div class="widget get-in-touch col-md-4 col-sm-6">
                 <h4 class="widget_title">Dove siamo</h4>
                 <div class="widget-contact-list">
@@ -16,12 +20,12 @@
                             <div class="fleft location_address">
                                 <b>Residence Uliveto</b><br>Via Monte Baldo, 35 - 37016 Garda (Vr), Italy
                             </div>
+
                         </li>
                         <li>
                             <i class="fa fa-phone"></i>
                             <div class="fleft contact_no">
-                                <a href="tel:+018655248503">Tel. +39 346 8689410</a> / <a href="tel:+12549547854">Fax
-                                    +39 045 6270477</a>
+                                <a href="tel:+018655248503">Tel. +39 346 8689410</a>  /  <a href="tel:+12549547854">Fax +39 045 6270477</a>
                             </div>
                         </li>
                         <li>
@@ -30,6 +34,7 @@
                                 <a href="mailto:info@resorthotel.com">info@residenceuliveto.it</a>
                             </div>
                         </li>
+
                     </ul>
                 </div>
             </div>
@@ -42,12 +47,12 @@
                             <div class="fleft location_address">
                                 <b>Residence Uliveto</b><br>Via Monte Baldo, 35 - 37016 Garda (Vr), Italy
                             </div>
+
                         </li>
                         <li>
                             <i class="fa fa-phone"></i>
                             <div class="fleft contact_no">
-                                <a href="tel:+018655248503">Tel. +39 346 8689410</a> / <a href="tel:+12549547854">Fax
-                                    +39 045 6270477</a>
+                                <a href="tel:+018655248503">Tel. +39 346 8689410</a>  /  <a href="tel:+12549547854">Fax +39 045 6270477</a>
                             </div>
                         </li>
                         <li>
@@ -56,20 +61,21 @@
                                 <a href="mailto:info@resorthotel.com">info@residenceuliveto.it</a>
                             </div>
                         </li>
+
                     </ul>
                 </div>
             </div>
         </section>
     </section>
     <section class="container clearfix footer-b-pad">
-        <div class="footer-copy">
-            <div class="pull-left fo-txt">
-                <p>Copyright © R.O.GY Sas di Gelmetti Giuditta e C. - P.IVA e C.F. 01648990230 Via Tiziano, 1/3 -37016
-                    Garda (Vr) </p>
+        <div class="footer-copy"><div class="pull-left fo-txt">
+                <p>Copyright © R.O.GY Sas di Gelmetti Giuditta e C. - P.IVA e C.F. 01648990230 Via Tiziano, 1/3 -37016 Garda (Vr) </p>
             </div>
             <div class="pull-right fo-txt">
                 <p>Design by: <a href="http://www.asten.it">Asten Srl</a></p>
             </div>
+
         </div>
     </section>
-</footer><!-- footer  style-->
+</footer>
+<!-- footer  style-->
